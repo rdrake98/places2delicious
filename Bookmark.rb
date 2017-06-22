92ee9f6f7da7f05b6397622d748fcc77f31f82de
Richard Drake <rdrake98@gmail.com>
2017-06-22 18:49:07 +0100

    still using yaml

--- Bookmark.rb
+++ Bookmark.rb
@@ -3,92 +3,30 @@ require 'rexml/document'
 require 'uri'
 require 'yaml'
 
-=begin
-* Name: Bookmark module and class
-* Description: a bookmark is a title, a url and tags
-* Author: Nicolas Meier
-* Creation Date: 2009-02-06
-* License: All Rights Reserved, Copyright © Nicolas Meier
-* Version 1.1 2009-02-06
-* Version 1.2 2012-03-03: separating tags by a comma
-=end
-module Bookmark
+CONFIG = YAML.load_file("places2delicious.yml")
 
-  CONFIG = YAML.load_file("places2delicious.yml")
+class Bookmark
+  VISIT_COUNT_SYMBOL = :"♞"
+  FRECENCY_SYMBOL =  :"♚"
 
-  class Bookmark
-    VISIT_COUNT_SYMBOL = :"♞"
-    FRECENCY_SYMBOL =  :"♚"
+  @@maxVisitCount = 0
+  @@maxFrecency = 0
+  attr_accessor :title, :url, :tags, :description, :visitCount, :frecency
 
-    @@maxVisitCount = 0
-    @@maxFrecency = 0
-    attr_accessor :title, :url, :tags, :description, :visitCount, :frecency
+  def initialize(title, url, tags, description, visitCount, frecency)
+    @title = title
+    @url = url
+    @tags = tags
+    @description = description
+    @visitCount = visitCount
+    @frecency = frecency
 
-    def initialize(title, url, tags, description, visitCount, frecency)
-      @title = title
-      @url = url
-      @tags = tags
-      @description = description
-      @visitCount = visitCount
-      @frecency = frecency
-
-      if @visitCount > @@maxVisitCount
-        @@maxVisitCount = @visitCount
-      end
-
-      if @frecency > @@maxFrecency
-        @@maxFrecency = @frecency
-      end
+    if @visitCount > @@maxVisitCount
+      @@maxVisitCount = @visitCount
     end
 
-    def postToDelicious(http, username, password, doNotShare)
-      if CONFIG["delicious"]["import-stats"]
-        visitCountTag = VISIT_COUNT_SYMBOL.to_s * (10 * @visitCount / @@maxVisitCount).ceil
-        frecencyTag = FRECENCY_SYMBOL.to_s * (10 * @frecency / @@maxFrecency).ceil
-        @tags.push(visitCountTag)
-        @tags.push(frecencyTag)
-      end
-
-      # Add the import tag
-      importTag = CONFIG["delicious"]["import-tag"]
-      if !importTag.nil? 
-        @tags.push(importTag)
-      end
-
-      # Prepare the arguments
-      encodedUrl = URI.escape(@url)
-      encodedUrl = URI.escape(encodedUrl, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-      postUrl = "?url=" + encodedUrl
-
-      encodedDesc = URI.escape(@title)
-      postDesc = "&description=" + encodedDesc
-
-      postExt = ""
-      if !@description.nil?
-        encodeExtended = URI.escape(@description)
-        postExt = "&extended=" + encodeExtended
-      end
-
-      postTags = "&tags=" + URI.escape(@tags.join(','))
-
-      post = postUrl + postDesc + postTags + postExt
-
-      # Keep the bookmark private ?
-      if doNotShare
-        post += "&shared=no"
-      end
-
-      # Create the HTTPs request
-      req = Net::HTTP::Get.new("/v1/posts/add" + post, {"User-Agent" => "juretta.com RubyLicious 0.2"})
-      req.basic_auth(username, password)
-
-      # Send the request
-      response = http.request(req)
-      resp = response.body
-
-      #  XML Document
-      doc = REXML::Document.new(resp)
-      doc.root.attributes['code'].upcase == "DONE"
+    if @frecency > @@maxFrecency
+      @@maxFrecency = @frecency
     end
   end
 end
--- places2delicious.rb
+++ places2delicious.rb
@@ -1,147 +1,64 @@
 #!/usr/bin/ruby
 
-require 'rubygems'
 require 'sqlite3'
-require 'net/https'
-require 'rexml/document'
-require 'uri'
-require_relative 'Bookmark'
 require 'yaml'
-require 'netrc'
 require 'inifile'
+require_relative 'Bookmark'
 
-=begin
-* Name: places2delicious
-* Description: export bookmarks from Firefox >3.x places.sqlite to delicious
-* Author: Nicolas Meier
-* Creation Date: 2009-02-06
-* License: All Rights Reserved, Copyright © Nicolas Meier
-* Version 1.2 2014-10-05
-=end
+class Places2delicious
+  pathToFirefox = "/Users/rd/Library/Application Support/Firefox/"
+  file = IniFile.load(pathToFirefox + "profiles.ini")
+  CONFIG = YAML.load_file("places2delicious.yml")
+  iniProfile = file["Profile" + CONFIG["places"]["profile-number"].to_s]
+  pathProfile = iniProfile["Path"]
+  pathProfile = pathToFirefox + pathProfile if iniProfile["IsRelative"]
+  @@pathToPlaces = pathProfile + "/places.sqlite"
 
-class Net::HTTP
-  alias_method :old_initialize, :initialize
-  def initialize(*args)
-    old_initialize(*args)
-    @ssl_context = OpenSSL::SSL::SSLContext.new
-    @ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  end
-end
+  def retrieveBookmarks
+      bookmarks = {}
+      # Open the places SQLite database
+      db = SQLite3::Database.open(@@pathToPlaces)
 
-module Places2delicious
-    class Places2delicious
-      CONFIG = YAML.load_file("places2delicious.yml")
-      n = Netrc.read
-    
-      # Get current user
-      currentUser = (%x(echo $USER)).delete("\n")
-      # Path to Firefox data and profiles.ini
-      pathToFirefox = "/Users/" + currentUser + "/Library/Application Support/Firefox/"
-      pathToProfilesIni = pathToFirefox + "profiles.ini"
-      
-      # Load profiles.ini file
-      file = IniFile.load(pathToProfilesIni)
-      
-      # Get the profile and its path
-      iniProfile = file["Profile" + CONFIG["places"]["profile-number"].to_s]
-      profilePath = iniProfile["Path"]
-      isRelative = iniProfile["IsRelative"]
-      if isRelative
-        pathProfile = pathToFirefox + profilePath
-      else
-        pathProfile = profilePath
-      end
-      
-      @@pathToPlaces = pathProfile + "/places.sqlite"
-      @@notSharedTag = CONFIG["places"]["not-shared-tag"]
-      @@apiUrl = CONFIG["delicious"]["api"]["url"]
-      @@apiPort = CONFIG["delicious"]["api"]["port"]
-      # Get delicious username and password from .netrc
-      @@username, @@password = n["delicious.com"]
+      # Query - Retrieve bookmarks associated to a place and to tags
+      query = "SELECT bookmark.title, place.url, place.title, tag.title, anno.content,
+      place.visit_count, place.frecency
 
-      def initialize
-      end
-      
-      def retrieveBookmarks
-          bookmarks = {}
-          
-          # Open the places SQLite database
-          db = SQLite3::Database.open(@@pathToPlaces)
-          
-          # Query - Retrieve bookmarks associated to a place and to tags
-          query = "SELECT bookmark.title, place.url, place.title, tag.title, anno.content,
-          place.visit_count, place.frecency
-          
-          FROM moz_bookmarks bookmark, moz_places place
-          
-          LEFT OUTER JOIN moz_bookmarks bt on bt.fk = bookmark.fk
-          LEFT OUTER JOIN moz_bookmarks tag on bt.parent = tag.id
-          LEFT OUTER JOIN moz_bookmarks tag_parent on tag.parent = tag_parent.id
-          LEFT OUTER JOIN moz_items_annos anno on bookmark.id = anno.item_id AND anno.anno_attribute_id = 2
-          AND tag_parent.title = 'Tags'
-          
-          WHERE place.id = bookmark.fk
-          AND bookmark.title is not null
-          AND bookmark.type = 1"
-          
-          # Execute the query
-          db.execute(query) do |row|
-            title = row[0]              # Bookmark title
-            url = row[1]                # Place url
-            tag = row[3]                # Bookmark tags
-            description = row[4]        # Description
-            visitCount = row[5].to_f    # Visit count
-            frecency = row[6].to_f      # Frecency
-          
-            array = [title, url]
-            bookmark = bookmarks[array]
-            if bookmark.nil?
-              bookmarks[array] = Bookmark::Bookmark.new(title, url, [tag], description, visitCount, frecency) # Create the Bookmark
-            else
-              bookmark.tags.push(tag) # Add the tags to the bookmark
-            end
-          end
-          
-          # Close the database connection
-          db.close
-          
-          bookmarks
-      end
-      
-      def importToDelicious tagToImport
-        i = 0
-        
-        # Open a TCP connection and an HTTPs session
-        http = Net::HTTP.new(@@apiUrl, @@apiPort)
-        http.use_ssl = true
-        http.start do |http|  
-        
-          bookmarks = retrieveBookmarks
-          nbOfBookmarks = bookmarks.values.size
-        
-          # Iterate over the retrieved bookmarks
-          bookmarks.values.each do |bookmark|
-            i += 1
-        
-            # Keep the bookmark private?
-            doNotShare = !(bookmark.tags.index(@@notSharedTag).nil?)
-        
-            # Import everything or Specific tag to process?
-            if tagToImport.nil? or !bookmark.tags.index(tagToImport).nil?
-              puts i.to_s + " / #{nbOfBookmarks}: #{bookmark.title} -> #{bookmark.url} : #{bookmark.tags.join(', ')}"
-        
-              puts bookmark.postToDelicious(http, @@username, @@password, doNotShare)
-            end
-          end
+      FROM moz_bookmarks bookmark, moz_places place
+
+      LEFT OUTER JOIN moz_bookmarks bt on bt.fk = bookmark.fk
+      LEFT OUTER JOIN moz_bookmarks tag on bt.parent = tag.id
+      LEFT OUTER JOIN moz_bookmarks tag_parent on tag.parent = tag_parent.id
+      LEFT OUTER JOIN moz_items_annos anno on bookmark.id = anno.item_id AND anno.anno_attribute_id = 2
+      AND tag_parent.title = 'Tags'
+
+      WHERE place.id = bookmark.fk
+      AND bookmark.title is not null
+      AND bookmark.type = 1"
+
+      # Execute the query
+      db.execute(query) do |row|
+        title = row[0]              # Bookmark title
+        url = row[1]                # Place url
+        tag = row[3]                # Bookmark tags
+        description = row[4]        # Description
+        visitCount = row[5].to_f    # Visit count
+        frecency = row[6].to_f      # Frecency
+
+        array = [title, url]
+        bookmark = bookmarks[array]
+        if bookmark.nil?
+          bookmarks[array] = Bookmark.new(title, url, [tag], description, visitCount, frecency)
+        else
+          bookmark.tags.push(tag) # Add the tags to the bookmark
         end
       end
 
-      if __FILE__ == $0
-        @@tagToImport = ARGV[0]
-  
-        instance = Places2delicious.new
-        instance.importToDelicious @@tagToImport
-      end
-      
-    end
+      # Close the database connection
+      db.close
+
+      bookmarks
+  end
 end
+instance = Places2delicious.new
+bookmarks = instance.retrieveBookmarks
+puts bookmarks.size
